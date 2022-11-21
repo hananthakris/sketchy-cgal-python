29385c196ee6b8397da64d4f1daca651127cf947
hananthakris <hananthakris@umass.edu>
2022-11-21 17:46:18 -0500

    All code done, blacked code

--- src/solver/nystrom_sketch.py
+++ src/solver/nystrom_sketch.py
@@ -1,40 +1,41 @@
 import numpy as np
 import sys
+
+
 class NystromSketch:
     def __init__(self, n, R, field):
         if R > n:
-            print('Sketch-size cannot be larger than the problem size.')
-        if field == 'real':
+            print("Sketch-size cannot be larger than the problem size.")
+        if field == "real":
             self.Omega = np.random.randn(n, R)
-        elif field == 'complex':
-            self.Omega = np.random.randn(n, R) + 1j *np.random.randn(n,R)
+        elif field == "complex":
+            self.Omega = np.random.randn(n, R) + 1j * np.random.randn(n, R)
         else:
-            print('Should be real or complex')
+            print("Should be real or complex")
         self.S = np.zeros(n, R)
 
     def reconstruct(self):
         eps = sys.float_info.epsilon
         S = self.S
-        n = np.size(self.S,1)
-        sigma = np.sqrt(n)* eps *max(np.linalg.norm(S, axis=-1))
-        S = S + sigma*self.Omega
+        n = np.size(self.S, 1)
+        sigma = np.sqrt(n) * eps * max(np.linalg.norm(S, axis=-1))
+        S = S + sigma * self.Omega
         B = self.Omega.T * S
-        B = 0.5*(B+B.T)
+        B = 0.5 * (B + B.T)
         if ~any(B[:]):
             Delta = 0
-            U = np.zeros(n,1)
+            U = np.zeros(n, 1)
         else:
             C = np.linalg.cholesky(B)
-            U, Sigma, temp = np.linalg.svd( S / C )
-            Delta = max(np.power(Sigma, 2) - sigma*np.eye(np.size(Sigma)), 0)
+            U, Sigma, temp = np.linalg.svd(S / C)
+            Delta = max(np.power(Sigma, 2) - sigma * np.eye(np.size(Sigma)), 0)
         return U, Delta
 
     def rank_one_update(self, v, eta):
-        self.S = (1-eta)*self.S + eta*(v*(v.T*self.Omega))
-
+        self.S = (1 - eta) * self.S + eta * (v * (v.T * self.Omega))
 
     def set(self, val):
-            if np.size(val) == np.size(self.S) or not self.S:
-                self.S = val
-            else:
-                print("Size of input does not match with sketch size")
+        if np.size(val) == np.size(self.S) or not self.S:
+            self.S = val
+        else:
+            print("Size of input does not match with sketch size")
--- src/utils/approx_eigenvectors.py
+++ src/utils/approx_eigenvectors.py
@@ -1,14 +1,15 @@
 import numpy as np
 import sys
 
+
 def ApproxMinEvecPower(M, n, q):
     if M.isnumeric():
-        M = lambda x: M*x
+        M = lambda x: M * x
 
-    Mnorm, cnt = powermethod(M,n,0.1)
-    shift = 2*Mnorm
+    Mnorm, cnt = powermethod(M, n, 0.1)
+    shift = 2 * Mnorm
     v = np.random.randn(n)
-    v = v/np.linalg.norm(v)
+    v = v / np.linalg.norm(v)
     Mv = M(v)
     cnt = cnt + 1
 
@@ -17,37 +18,38 @@ def ApproxMinEvecPower(M, n, q):
         v = v / np.linalg.norm(v)
         Mv = M(v)
         cnt = cnt + 1
-    xi = v.getH()*Mv
+    xi = v.getH() * Mv
     return v, xi, cnt
 
 
 def powermethod(S, n, tol):
     cnt = 0
     x = np.random.randn(n)
-    x = x/np.linalg.norm(x)
+    x = x / np.linalg.norm(x)
     e = 1
     e0 = 0
-    while abs(e-e0) > tol*e:
+    while abs(e - e0) > tol * e:
         e0 = e
         Sx = S(x)
         if np.count_nonzero(Sx) == 0:
             Sx = np.random.rand(np.size(Sx))
         x = S(Sx)
         normx = np.linalg.norm(x)
-        e = normx/np.linalg.norm(Sx)
-        x = x/normx
-        cnt = cnt+1
+        e = normx / np.linalg.norm(Sx)
+        x = x / normx
+        cnt = cnt + 1
         if cnt > 100:
-            print('Power method did not converge')
+            print("Power method did not converge")
 
     return e, cnt
 
-def ApproxMinEvecLanczos(M, n, q):
-    q = min(q, n-1)
-    if M.isnumeric():
-        M = lambda x: M*x
 
-    Q = np.zeros((n, q+1))
+def ApproxMinEvecLanczos(M, n, q):
+    q = min(q, n - 1)
+    if M.isnumeric():
+        M = lambda x: M * x
+
+    Q = np.zeros((n, q + 1))
 
     aleph = np.zeros(q)
     beth = np.zeros(q)
@@ -57,30 +59,34 @@ def ApproxMinEvecLanczos(M, n, q):
 
     i = 0
     for i in range(q):
-        Q[:, i+1] = M(Q[:, i])
-        aleph[i] = np.real(Q[:,i].getH() * Q[:i+1])
-        if (i == 1):
-            Q[:, i+1] -= aleph[i] * Q[:,i]
+        Q[:, i + 1] = M(Q[:, i])
+        aleph[i] = np.real(Q[:, i].getH() * Q[: i + 1])
+        if i == 1:
+            Q[:, i + 1] -= aleph[i] * Q[:, i]
         else:
-            Q[:, i+1] = Q[:, i+1] - aleph[i] * Q[:, i] - beth[i-1] * Q[:, i-1]
+            Q[:, i + 1] = Q[:, i + 1] - aleph[i] * Q[:, i] - beth[i - 1] * Q[:, i - 1]
 
+        beth[i] = np.linalg.norm(Q[:, i + 1])
 
-        beth[i] = np.linalg.norm(Q[:, i+1])
-
-        if (abs(beth[i]) < np.sqrt(n)*sys.float_info.epsilon):
+        if abs(beth[i]) < np.sqrt(n) * sys.float_info.epsilon:
             break
 
-        Q[:, i+1] /= beth[i]
+        Q[:, i + 1] /= beth[i]
 
-    B = np.diagonal(aleph[0:i], 0) + np.diagonal(beth[1:[i-1]], +1) + np.diagonal(beth[1:[i-1]], -1)
+    B = (
+        np.diagonal(aleph[0:i], 0)
+        + np.diagonal(beth[1 : [i - 1]], +1)
+        + np.diagonal(beth[1 : [i - 1]], -1)
+    )
 
-    U, D = cgal_eig(0.5*(B+np.transpose(B)))
+    U, D = cgal_eig(0.5 * (B + np.transpose(B)))
     [xi, ind] = min(D)
     v = Q[:, :i] * U[:, ind]
     nv = np.linalg.norm(v)
-    xi = xi*nv
-    v = v/nv
-    return v,xi,i
+    xi = xi * nv
+    v = v / nv
+    return v, xi, i
+
 
 def cgal_eig(X):
     try:
@@ -91,6 +97,7 @@ def cgal_eig(X):
         D, ind = np.sort(D)
         V = V[:, ind]
     return V, D
+
 
 def ApproxMinEvecLanczosSE(M, n, q):
     pass
